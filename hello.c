7a46a213b0e6ee467b17147856aea685ac983870
Nrussell2001 <nrussell7@huskers.unl.edu>
2022-08-25 18:31:19 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Noah Russell
+ *         nrussell7@huskers.unl.edu
+ * Date: 2022/08/25
+ *
+ * Hello World Program Hack01
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv)
+ {
+    printf("Hello World\n");
+
+    return 0;
+ }
